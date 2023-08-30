ee656a4f38f50f4adb81fdb0170c673c0c2975e3
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-08-30 12:48:55 -0700

    Implement select menu appearing on click

---
+++ client/src/components/SelectMenu.tsx
@@ -0,0 +1,36 @@
+import { List, ListItem, ListItemButton, ListItemContent } from '@mui/joy';
+import { FC, useEffect } from 'react';
+
+interface IProps {
+  x: number;
+  y: number;
+  clicked: boolean;
+}
+
+const SelectMenu: FC<IProps> = ({ x, y, clicked }) => {
+  useEffect(() => {
+    // fetch targets from db ? or in Game.tsx
+  }, [x, y]);
+
+  return (
+    <List sx={{ visibility: clicked ? 'visible' : 'hidden', position: 'absolute', top: `${y}px`, left: `${x}px`, zIndex: 1000 }}>
+      <ListItem>
+        <ListItemButton variant="soft">
+          <ListItemContent>TARGET 1</ListItemContent>
+        </ListItemButton>
+      </ListItem>
+      <ListItem>
+        <ListItemButton variant="soft">
+          <ListItemContent>TARGET 2</ListItemContent>
+        </ListItemButton>
+      </ListItem>
+      <ListItem>
+        <ListItemButton variant="soft">
+          <ListItemContent>TARGET 3</ListItemContent>
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+};
+
+export default SelectMenu;
--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,24 +1,31 @@
-import { FC, useState, MouseEvent } from 'react';
+import { FC, useState, MouseEvent, useRef } from 'react';
 import useTimer from '../utils/useTimer';
-import { AspectRatio, Dropdown, Menu, MenuButton, MenuItem, Skeleton, Stack, Typography } from '@mui/joy';
+import { AspectRatio, Skeleton, Stack, Typography } from '@mui/joy';
 import { useLocation } from 'react-router-dom';
+import SelectMenu from '../components/SelectMenu';
 
 const Game: FC = () => {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [x, setX] = useState(-1);
+  const [y, setY] = useState(-1);
+  const [clicked, setClicked] = useState(false);
 
   const [correctCount, setCorrectCount] = useState(0);
   const [timerStarted, setTimerStarted] = useState(false);
   const [loading, setLoading] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
 
   const { minutes, seconds } = useTimer(timerStarted);
   const { state } = useLocation();
 
   const handleImageClick = (e: MouseEvent<HTMLImageElement>) => {
-    setX(e.clientX);
-    setY(e.clientY);
+    // prevent menu from going off-screen on edges
+    if (imgRef && imgRef.current) {
+      imgRef.current.offsetWidth - e.pageX < 144 ? setX(e.pageX - 144) : setX(e.pageX);
+      imgRef.current.offsetHeight - e.pageY < 143 ? setY(e.pageY - 143) : setY(e.pageY);
+      setClicked((prevClicked) => !prevClicked);
+    }
 
-    console.log(`Clicked on (${e.clientX}, ${e.clientY})`);
+    console.log(`Clicked on (${e.pageX}, ${e.pageY})`);
   };
 
   return (
@@ -30,20 +37,12 @@ const Game: FC = () => {
         </Typography>
         <Typography level="body-md">Targets</Typography>
       </Stack>
-      <Dropdown>
-        <AspectRatio ratio={'3/2'} sx={{ width: '100%' }}>
-          <Skeleton loading={loading}>
-            <MenuButton>
-              <img src={state.link} alt={state.name} onClick={(e) => handleImageClick(e)} />
-            </MenuButton>
-          </Skeleton>
-        </AspectRatio>
-        <Menu>
-          <MenuItem>TARGET 1</MenuItem>
-          <MenuItem>TARGET 2</MenuItem>
-          <MenuItem>TARGET 3</MenuItem>
-        </Menu>
-      </Dropdown>
+      <SelectMenu x={x} y={y} clicked={clicked} />
+      <AspectRatio ratio={'3/2'} sx={{ width: '100%' }}>
+        <Skeleton loading={loading}>
+          <img src={state.link} alt={state.name} ref={imgRef} onClick={(e) => handleImageClick(e)} />
+        </Skeleton>
+      </AspectRatio>
     </Stack>
   );
 };
